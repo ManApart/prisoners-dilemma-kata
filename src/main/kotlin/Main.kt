213c42c54687224fa7dfafee372a2076ef805df0
ManApart <raustinkucera@gmail.com>
2020-03-06 09:36:52 -0500

    add main method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,18 @@
-fun main(args: List<String>) {
- println("Hello World")
+import java.util.*
+
+fun main(args: Array<String>) {
+    val prisonerCount = args.firstOrNull()?.toInt() ?: 10
+    val jail = Jail(prisonerCount)
+    var wardenVisits = 0
+    val rand = Random()
+    while (!jail.warden.hasBeenGivenCorrectGuess && wardenVisits < 100000000) {
+        val prisoner = jail.prisoners[rand.nextInt(jail.prisoners.size)]
+        jail.warden.summon(prisoner)
+        wardenVisits++
+    }
+    if (jail.warden.hasBeenGivenCorrectGuess) {
+        println("Guessed correctly after $wardenVisits visits to the warden.")
+    } else {
+        println("Never guessed correctly!")
+    }
 }
